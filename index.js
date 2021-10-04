91e5756c3cc1d65f51997b0a5350b8d0927b0d4b
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2021-10-04 18:45:42 -0300

    commit

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 const pokedex = [
     {numero: 001, nome:"Bulbasaur", tipo: "grass", imagem: src="/img/Bulbasaur.png", descricao: "Pokemon inicial de Kanto", altura: 0.71, peso:6.9, categoria: "seed pokemon", habilidade:"overgrow"},
-    {numero: 004, nome:"Charmander", tipo: "fire", imagem: href="/img/Charmander.png", descricao: "Pokemon inicial de Kanto", altura: 0.61, peso:8.5, categoria: "lizard pokemon", habilidade:"Blaze"},
-    {numero: 007, nome:"Squirtle", tipo: "water", imagem: href="/img/Squirtle.png", descricao: "Pokemon inicial de Kanto", altura: 0.51, peso:9, categoria: "Tiny turtle pokemon", habilidade:"overgrow"}
+    {numero: 004, nome:"Charmander", tipo: "fire", imagem: src="/img/Charmander.png", descricao: "Pokemon inicial de Kanto", altura: 0.61, peso:8.5, categoria: "lizard pokemon", habilidade:"Blaze"},
+    {numero: 007, nome:"Squirtle", tipo: "water", imagem: scr="/img/Squirtle.png", descricao: "Pokemon inicial de Kanto", altura: 0.51, peso:9, categoria: "Tiny turtle pokemon", habilidade:"overgrow"}
 ];
 
 app.set("view engine", "ejs");
@@ -16,29 +16,29 @@ var message ="";
 var poke;
 app.get("/", (req, res) => {
     setTimeout(() => {message = "";}, 1000);
-    res.render("index", { pokedex: pokedex, message:message });
+    res.render("index", { pokedex: pokedex, message:message, poke });
   })
 app.get("/index", (req, res) => {
     setTimeout(() => {message = "";}, 1000);
-    res.render("index", { pokedex: pokedex, message:message});
+    res.render("index", { pokedex: pokedex, message:message, poke});
 });
 
 app.get("/cadastro", (req, res) => {
     res.render("cadastro");
 });
 app.get("/Pokedex", (req, res) => {
-    res.render("Pokedex", {pokedex:pokedex});
+    res.render("Pokedex", {pokedex:pokedex, poke});
 });
 
 app.get("/detalhes/:ind", (req, res) => {
-    const indice = req.params.ind;
-    poke = pokedex[indice];
-    res.render("detalhes", { pokedex: pokedex,poke});
+    var indice = req.params.ind;
+var poke = pokedex[indice]; 
+    res.render("detalhes", { pokedex: pokedex,poke:poke});
 });
 
 app.post ( "/form",  ( req ,  res )  =>  {
-    const  { nome , tipo , imagem , descricao , altura , peso , categoria , habilidade } = req.body ;
-    pokedex.push( {nome ,tipo ,imagem ,descricao , altura ,  peso ,  categoria ,  habilidade } ) ; 
+    const  { nome , numero, tipo , imagem , descricao , altura , peso , categoria , habilidade } = req.body ;
+    pokedex.push( {nome , numero,tipo ,imagem ,descricao , altura ,  peso ,  categoria ,  habilidade } ) ; 
     message  =  "Pokémon cadastrado" ;
     res.redirect ( "/" );
   } ) ;
