a9ea909cd4559502db20c55f5eeef612cef99207
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2021-10-03 22:49:40 -0300

    commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+const pokedex = [
+    {numero: 001, nome:"Bulbasaur", tipo: "grass", imagem: src="/img/Bulbasaur.png", descricao: "Pokemon inicial de Kanto", altura: 0.71, peso:6.9, categoria: "seed pokemon", habilidade:"overgrow"},
+    {numero: 004, nome:"Charmander", tipo: "fire", imagem: href="/img/Charmander.png", descricao: "Pokemon inicial de Kanto", altura: 0.61, peso:8.5, categoria: "lizard pokemon", habilidade:"Blaze"},
+    {numero: 007, nome:"Squirtle", tipo: "water", imagem: href="/img/Squirtle.png", descricao: "Pokemon inicial de Kanto", altura: 0.51, peso:9, categoria: "Tiny turtle pokemon", habilidade:"overgrow"}
+];
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded());
+
+var message ="";
+var poke;
+app.get("/", (req, res) => {
+    setTimeout(() => {message = "";}, 1000);
+    res.render("index", { pokedex: pokedex, message:message });
+  })
+app.get("/index", (req, res) => {
+    setTimeout(() => {message = "";}, 1000);
+    res.render("index", { pokedex: pokedex, message:message});
+});
+
+app.get("/cadastro", (req, res) => {
+    res.render("cadastro");
+});
+
+app.get("/detalhes/:ind", (req, res) => {
+    const indice = req.params.ind;
+    poke = pokedex[indice];
+    res.render("detalhes", { pokedex: pokedex,poke});
+});
+
+app.post ( "/form",  ( req ,  res )  =>  {
+    const  { nome , tipo , imagem , descricao , altura , peso , categoria , habilidade } = req.body ;
+    pokedex.push( {nome ,tipo ,imagem ,descricao , altura ,  peso ,  categoria ,  habilidade } ) ; 
+    message  =  "Pokémon cadastrado" ;
+    res.redirect ( "/" );
+  } ) ;
+
+app.listen(port, () =>
+  console.log(`Servidor rodando em http://localhost:${port}`)
+);
